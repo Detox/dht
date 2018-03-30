16013da13d78a8c47227e1d0e002495f7d7f2874
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2018-03-31 01:46:18 +0300

    Updated to latest version of `webtorrent-dht` 0.16.1. Stopped including `simple-peer` with browser bundle, it needs to be loaded separately and supplied explicitly.

--- gulpfile.js
+++ gulpfile.js
@@ -3,4 +3,4 @@
  * @author  Nazar Mokrynskyi <nazar@mokrynskyi.com>
  * @license 0BSD
  */
-require('build-gbu')('src/index.js', 'detox_dht', 'detox-dht.browser', 'simple-sha1');
+require('build-gbu')('src/index.js', 'detox_dht', 'detox-dht.browser', ['simple-sha1', 'simple-peer']);
