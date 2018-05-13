72017d60e13b5660307f9c8b26d110854d9ffb01
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2018-05-13 20:05:59 +0300

    Added skeleton for future DHT implementation with approximate set of public methods and arguments (based on methods used from WebTorrent DHT-based implementation)

--- src/index.js
+++ src/index.js
@@ -5,13 +5,72 @@
  * @license 0BSD
  */
 (function(){
-  var bencode, webrtcSocket, webtorrentDht;
-  bencode = require('bencode');
-  webrtcSocket = require('webtorrent-dht/webrtc-socket');
-  webtorrentDht = require('webtorrent-dht');
-  module.exports = {
-    'bencode': bencode,
-    'webrtc-socket': webrtcSocket,
-    'webtorrent-dht': webtorrentDht
-  };
+  function Wrapper(asyncEventer, esDht){
+    /**
+     * @constructor
+     *
+     * @param {!Uint8Array}	id									Own ID
+     * @param {!Function}	hash_function						Hash function to be used for Merkle Tree
+     * @param {number}		bucket_size							Size of a bucket from Kademlia design
+     * @param {number}		state_history_size					How many versions of local history will be kept
+     * @param {number}		fraction_of_nodes_from_same_peer	Max fraction of nodes originated from single peer allowed on lookup start
+     *
+     * @return {!DHT}
+     */
+    function DHT(id, hash_function, bucket_size, state_history_size, fraction_of_nodes_from_same_peer){
+      fraction_of_nodes_from_same_peer == null && (fraction_of_nodes_from_same_peer = 0.2);
+      if (!(this instanceof DHT)) {
+        return new DHT(id, hash_function, bucket_size, state_history_size, fraction_of_nodes_from_same_peer);
+      }
+      asyncEventer.call(this);
+    }
+    DHT.prototype = {
+      /**
+       * @param {string}	ip
+       * @param {number}	port
+       */
+      'listen': function(ip, port){}
+      /**
+       * @param {!Uint8Array} id
+       *
+       * @return {!Promise}
+       */,
+      'lookup': function(id){}
+      /**
+       * @return {!Array<!Uint8Array>}
+       */,
+      'get_peers': function(){}
+      /**
+       * @param {!Uint8Array} key
+       *
+       * @return {!Promise}
+       */,
+      'get': function(key){}
+      /**
+       * @param {!Uint8Array} data
+       *
+       * @return {!Uint8Array} Key
+       */,
+      'put_immutable': function(data){}
+      /**
+       * @param {!Uint8Array} public_key
+       * @param {!Uint8Array} data
+       * @param {!Uint8Array} signature
+       */,
+      'put_mutable': function(public_key, data, signature){},
+      'destroy': function(){}
+    };
+    DHT.prototype = Object.assign(Object.create(asyncEventer.prototype), DHT.prototype);
+    Object.defineProperty(DHT.prototype, 'constructor', {
+      value: DHT
+    });
+    return DHT;
+  }
+  if (typeof define === 'function' && define['amd']) {
+    define(['async-eventer', 'es-dht'], Wrapper);
+  } else if (typeof exports === 'object') {
+    module.exports = Wrapper(require('async-eventer'), require('es-dht'));
+  } else {
+    this['detox_dht'] = Wrapper(this['async_eventer'], this['es_dht']);
+  }
 }).call(this);
--- src/index.ls
+++ src/index.ls
@@ -3,11 +3,70 @@
  * @author  Nazar Mokrynskyi <nazar@mokrynskyi.com>
  * @license 0BSD
  */
-bencode			= require('bencode')
-webrtc-socket	= require('webtorrent-dht/webrtc-socket')
-webtorrent-dht	= require('webtorrent-dht')
+function Wrapper (async-eventer, es-dht)
+	/**
+	 * @constructor
+	 *
+	 * @param {!Uint8Array}	id									Own ID
+	 * @param {!Function}	hash_function						Hash function to be used for Merkle Tree
+	 * @param {number}		bucket_size							Size of a bucket from Kademlia design
+	 * @param {number}		state_history_size					How many versions of local history will be kept
+	 * @param {number}		fraction_of_nodes_from_same_peer	Max fraction of nodes originated from single peer allowed on lookup start
+	 *
+	 * @return {!DHT}
+	 */
+	!function DHT (id, hash_function, bucket_size, state_history_size, fraction_of_nodes_from_same_peer = 0.2)
+		if !(@ instanceof DHT)
+			return new DHT(id, hash_function, bucket_size, state_history_size, fraction_of_nodes_from_same_peer)
+		async-eventer.call(@)
 
-module.exports	=
-	'bencode'			: bencode
-	'webrtc-socket'		: webrtc-socket
-	'webtorrent-dht'	: webtorrent-dht
+	DHT:: =
+		/**
+		 * @param {string}	ip
+		 * @param {number}	port
+		 */
+		'listen' : (ip, port) ->
+		/**
+		 * @param {!Uint8Array} id
+		 *
+		 * @return {!Promise}
+		 */
+		'lookup' : (id) ->
+		/**
+		 * @return {!Array<!Uint8Array>}
+		 */
+		'get_peers' : ->
+		/**
+		 * @param {!Uint8Array} key
+		 *
+		 * @return {!Promise}
+		 */
+		'get' : (key) ->
+		/**
+		 * @param {!Uint8Array} data
+		 *
+		 * @return {!Uint8Array} Key
+		 */
+		'put_immutable' : (data) ->
+		/**
+		 * @param {!Uint8Array} public_key
+		 * @param {!Uint8Array} data
+		 * @param {!Uint8Array} signature
+		 */
+		'put_mutable' : (public_key, data, signature) !->
+		'destroy' : ->
+
+	DHT:: = Object.assign(Object.create(async-eventer::), DHT::)
+	Object.defineProperty(DHT::, 'constructor', {value: DHT})
+
+	DHT
+
+if typeof define == 'function' && define['amd']
+	# AMD
+	define(['async-eventer', 'es-dht'], Wrapper)
+else if typeof exports == 'object'
+	# CommonJS
+	module.exports = Wrapper(require('async-eventer'), require('es-dht'))
+else
+	# Browser globals
+	@'detox_dht' = Wrapper(@'async_eventer', @'es_dht')
